a5d7f9ed6d1af8cb01741fe1368bddc8450d6224
kabin55 <shresthakabin46@gmail.com>
2025-12-26 22:14:16 +0545

    Port changed

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use("/api/auth", require("./routes/authRoutes"));
     res.sendFile(path.join(clientPath, "index.html"));
   });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
